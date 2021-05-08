9978e1e682a39dfe52efae6be2307eeb22923487
mohaksin <mohaksin@cisco.com>
2021-05-08 19:39:19 +0530

    Added animation

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import {Animation} from '../Animation';
+import './Home.css';
 const Home = (props:any) => {
+
+  const [showAnimation, setShowAnimation] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowAnimation(false);
+    }, 3000)
+  }, []);
+
+
   return (
-    <div>
-      <h3>Home View</h3>
-      <p> This is the home view of SPA</p>
-    </div>
-  );
+    showAnimation ? <Animation /> : <> <div>
+    <h3>Home View</h3>
+    <p> This is the home view of SPA</p>
+  </div></>
+  )
 };
 
 export default Home;
