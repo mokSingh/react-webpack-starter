219ee9a9dadf433b197bac87a813035c696de886
mohaksin <mohaksin@cisco.com>
2021-05-08 16:24:01 +0530

    Adding momentum on top of it

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "@momentum-ui/web-components";
 
 const HelloWorld = () => {
     return (
         <h1>
-            Hello World
+         <md-button variant="primary" tab-index="-1" color="violet"><span slot="text">no disabled</span></md-button>
         </h1>
     );
 }
