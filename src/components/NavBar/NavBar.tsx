426f4fbb487d5864e05e9515d6c5d6880f1de6e8
mohaksin <mohaksin@cisco.com>
2021-05-08 18:21:19 +0530

    navigation clean up

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import "@momentum-ui/web-components";
 import './NavBar.css';
 
 const NavBar = () => {
   return (
-    <div>
-      <h5>NAVBAR</h5>
+    <header className="header">
       <ul>
         <li><Link to="/Home">Home</Link></li>
         <li><Link to="/About">About</Link></li>
-     	</ul>
-      <hr />
-    </div>
+      </ul>
+      <div className="avatar"> <md-avatar alt="avatar" title="Mohak Singh" size="20" type="active" src="../../images/mohak.png"></md-avatar></div>
+    </header>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes } from './routes'; // where we are going to specify our routes
+import "@momentum-ui/web-components";
 
 ReactDOM.render(
   <Router>
